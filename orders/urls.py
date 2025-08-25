210fa2defe2bef7957a741385bca63c23df863a3
Talib Husain <talib.raath@gmail.com>
2025-08-25 16:15:37 +0500

    order management

---
+++ orders/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import PlaceOrderViewSet
+
+router = DefaultRouter()
+
+router.register(r'', PlaceOrderViewSet, basename='order')
+
+urlpatterns = [
+
+] + router.urls
