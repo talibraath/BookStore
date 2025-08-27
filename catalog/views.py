ed8a55ca08e2c6c1fc24b663197c8f2db2d1bd38
Talib Husain <talib.raath@gmail.com>
2025-08-27 13:30:07 +0500

    Testcases for catalog endpoints

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
             return True
         return request.user.is_authenticated and request.user.role == 'admin'
 
