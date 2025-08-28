a6d5a508ff486f62a5eb1b346dc12a2fb3daa4cf
Talib Husain <talib.raath@gmail.com>
2025-08-28 14:16:28 +0500

    Fixes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -70,7 +70,7 @@ REST_FRAMEWORK = {
     ),
 
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10, 
+    "PAGE_SIZE": 8, 
 
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',  # For unauthenticated users
