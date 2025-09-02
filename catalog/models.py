3b3e046ce388113f35a09fda39bec11a82d8a0e7
Talib Husain <107783888+talibraath@users.noreply.github.com>
2025-09-02 13:20:16 +0500

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -23,8 +23,8 @@ class Book(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="books")
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="books")
     pub_date = models.DateField()
     
     def __str__(self):
