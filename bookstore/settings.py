6cb87b52a0ba4ed2666160c911a1d969fded1cf5
Talib Husain <talib.raath@gmail.com>
2025-09-04 11:05:56 +0500

    Redis url fixes for deployment

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -218,7 +218,8 @@ SWAGGER_SETTINGS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        # "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": config("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
